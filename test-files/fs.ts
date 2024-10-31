36a94cad567c479a6b3f2137ce139be27f99a11b
Carlos Galarza <carloslfu@gmail.com>
2024-10-31 12:07:08 -0500

    organize test-files

--- test-files/fs.ts
+++ test-files/fs.ts
@@ -2,7 +2,6 @@ const text = `Hello, World!`;
 const encoder = new TextEncoder();
 const data = encoder.encode(text);
 
-console.log("Writing file data", data);
-
 await Deno.writeFile("hello.txt", data);
+
 console.log("File written successfully!");
