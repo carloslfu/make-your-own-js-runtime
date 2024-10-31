593157500f9f3460385bc5362b1a0ad4993973a2
Carlos Galarza <carloslfu@gmail.com>
2024-10-31 11:26:57 -0500

    custom prompter!

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Deno Runtime Rust
 
-Run JavaScript from Rust using the Deno Runtime crate.
+Run JavaScript from Rust using the Deno Runtime crate. This repo showcases:
+- Importing from https sources like esm.sh and handling TypeScript files
+- Handling permissions with custom prompter
+- Writing extensions
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,11 @@ use deno_runtime::deno_core::error::AnyError;
 use deno_runtime::deno_core::op2;
 use deno_runtime::deno_core::ModuleSpecifier;
 use deno_runtime::deno_fs::RealFs;
+use deno_runtime::deno_permissions::set_prompter;
+use deno_runtime::deno_permissions::PermissionPrompter;
+use deno_runtime::deno_permissions::Permissions;
 use deno_runtime::deno_permissions::PermissionsContainer;
+use deno_runtime::deno_permissions::PromptResponse;
 use deno_runtime::permissions::RuntimePermissionDescriptorParser;
 use deno_runtime::worker::MainWorker;
 use deno_runtime::worker::WorkerOptions;
@@ -32,6 +36,25 @@ deno_runtime::deno_core::extension!(
   esm = [dir "src", "bootstrap.js"]
 );
 
+struct CustomPrompter;
+
+impl PermissionPrompter for CustomPrompter {
+    fn prompt(
+        &mut self,
+        _message: &str,
+        _name: &str,
+        _api_name: Option<&str>,
+        _is_unary: bool,
+    ) -> PromptResponse {
+        println!(
+            "Prompting for permission, message: {}, name: {}, api_name: {:?}, is_unary: {}",
+            _message, _name, _api_name, _is_unary
+        );
+
+        PromptResponse::Allow
+    }
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), AnyError> {
     let js_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("./fs.ts");
@@ -42,6 +65,11 @@ async fn main() -> Result<(), AnyError> {
 
     let source_map_store = Rc::new(RefCell::new(HashMap::new()));
 
+    let permission_container =
+        PermissionsContainer::new(permission_desc_parser, Permissions::none_with_prompt());
+
+    set_prompter(Box::new(CustomPrompter));
+
     let mut worker = MainWorker::bootstrap_from_options(
         main_module.clone(),
         WorkerServiceOptions {
@@ -50,7 +78,7 @@ async fn main() -> Result<(), AnyError> {
             }),
             // File only loader
             // module_loader: Rc::new(FsModuleLoader),
-            permissions: PermissionsContainer::allow_all(permission_desc_parser),
+            permissions: permission_container,
             blob_store: Default::default(),
             broadcast_channel: Default::default(),
             feature_checker: Default::default(),
